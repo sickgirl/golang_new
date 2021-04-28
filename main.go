1ad1b29f5f160b3b7a0a6c2e7718255ceb721651
gao.shan_2305 <gao.shan_2305>
2021-04-28 10:23:51 +0800

    111

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
-package mian
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 	fmt.Println("hello world!")
 }
